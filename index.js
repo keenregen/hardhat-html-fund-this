2517a78003d53b1b7d4f8a8602c0690272f897b1
Keen Regen <keenregen@gmail.com>
2023-02-10 12:38:35 +0300

    v1.1

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const connect = async () => {
 };
 
 const fund = async () => {
-     const ethAmount = "0.1";
+     const ethAmount = document.getElementById("ethAmount").value;
      console.log(`Funding ${ethAmount}`);
      if (typeof window.ethereum !== "undefined") {
           const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -57,7 +57,39 @@ function listenForTransactionMine(transactionResponse, provider) {
      });
 }
 
+const getBalance = async () => {
+     if (typeof window.ethereum !== "undefined") {
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          try {
+               const balance = await provider.getBalance(contractAddress);
+               console.log(ethers.utils.formatEther(balance));
+          } catch (error) {
+               console.log(error);
+          }
+     } else {
+          balanceButton.innerHTML = "Please install MetaMask";
+     }
+};
+
+const withdraw = async () => {
+     console.log(`Withdrawing...`);
+     if (typeof window.ethereum !== "undefined") {
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(contractAddress, abi, signer);
+          try {
+               const transactionResponse = await contract.withdraw();
+               await listenForTransactionMine(transactionResponse, provider);
+               // await transactionResponse.wait(1)
+          } catch (error) {
+               console.log(error);
+          }
+     } else {
+          withdrawButton.innerHTML = "Please install MetaMask";
+     }
+};
+
 connectButton.onclick = connect;
+balanceButton.onclick = getBalance;
 fundButton.onclick = fund;
-// withdrawButton.onclick = withdraw;
-// balanceButton.onclick = getBalance;
+withdrawButton.onclick = withdraw;
